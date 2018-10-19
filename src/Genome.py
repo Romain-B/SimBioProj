3a2a7c6d677393ac470d7d9e1a33617d460da577
Romain-B <rbulteau@bs406-s33-01.insa-lyon.fr>
2018-10-19 09:58:11 +0200

    Added methods for distance from obj and inv pos check

--- src/Genome.py
+++ src/Genome.py
@@ -14,7 +14,7 @@ class Genome(object):
 		TSS = pd.read_table(path_init+'/TSS.dat', header=0)
 		TTS = pd.read_table(path_init+'/TTS.dat', header=0)
 		self.gene_info = TSS.merge(TTS)
-		self.prot = pd.read_table(path_init+'/prot.dat', header=0)['prot_pos']
+		self.prot = pd.read_table(path_init+'/prot.dat', header=0)
 		self.env = pd.read_table(path_init+'/environment.dat')
 
 	def __str__(self):
@@ -23,6 +23,67 @@ class Genome(object):
 		s += "Gene Info : \n"+str(self.gene_info)+"\n\n"
 		s += "Barriers : \n"+str(self.prot)+"\n\n"	
 		return s
+
+
+	def nearest_obj_distance(self, p):
+		# Needed to ensure that genes and barriers don't become less that sec_dist apart.
+		# dist gene, dist barrier
+		d_g = self.Size
+		d_b = self.Size
+
+		for i, row in self.gene_info.iterrows():
+			if abs(row['TSS_pos'] - p) < d_g:
+				d_g = abs(row['TSS_pos'] - p)
+			if abs(row['TTS_pos'] - p) < d_g:
+				d_g = abs(row['TTS_pos'] - p)
+
+		for i, row in self.prot.iterrows():
+			if abs(row['prot_pos'] - p) < d_b:
+				d_b = abs(row['prot_pos'] - p)
+
+		
+		####### for extremities of genome cases :
+
+		# max pos of a gene
+		gmax = max(max(self.gene_info['TTS_pos']),max(self.gene_info['TSS_pos'])) 
+		#idem min
+		gmin = min(min(self.gene_info['TTS_pos']),min(self.gene_info['TSS_pos']))
+
+
+		if p > gmax :
+			if self.Size-p+gmin < d_g:
+				d_g = self.Size-p+gmin
+
+		if p < gmin :
+			if p+self.Size-gmax < d_g:
+				d_g = p+self.Size-gmax
+
+
+		# same for barriers
+		bmax = max(self.prot['prot_pos']) 
+		bmin = min(self.prot['prot_pos']) 
+
+		if p > bmax :
+			if self.Size-p+bmin < d_b:
+				d_b = self.Size-p+bmin
+
+		if p < bmin :
+			if p+self.Size-bmax < d_b:
+				d_b = p+self.Size-bmax
+
+		return d_g, d_b
+
+
+	def good_inv_pos(self, s, e):
+		# checks is inversion positions are ok.
+		
+		s_gd, s_bd = self.nearest_obj_distance(s)
+		e_gd, e_bd = self.nearest_obj_distance(e)
+
+		return True if s_gd+e_bd > self.sec_dist and s_bd+e_gd > self.sec_dist else False
+
+
+
 
 
 
