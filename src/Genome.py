c04707e071cea5b59a7e7208460bb770c2ea7c08
Romain-B <rbulteau@bs406-s33-01.insa-lyon.fr>
2018-10-19 09:01:20 +0200

    Added file reading and print of Genome obj

--- src/Genome.py
+++ src/Genome.py
@@ -1,26 +1,38 @@
+#/usr/bin/env python
+#coding:utf-8
+
 import pandas as pd
 import numpy as np
 
-class Gene:
-	"""docstring for Genome"""
-
-	def __init__(self, s,e,ori,prom_str):
-		self.s = s
-		self.e = e
-		self.ori = ori
-		self.prom_str = prom_str
-
 
 class Genome(object):
-	"""docstring for Genome
-"""
-	def __init__(self, Size,path_to_env,path_tss,path_tts,path_prot):
-		self.Size = Size
-		st_genes = pd.read_table(path_tss)
-		#self.genes = 
 
-		self.env = pd.read_table(path_to_env)
-	
-	def 
+	def __init__(self, Size, path_init):
+		self.Size = Size
+		self.sec_dist = 60 # security distance of prot and genes
+
+		TSS = pd.read_table(path_init+'/TSS.dat', header=0)
+		TTS = pd.read_table(path_init+'/TTS.dat', header=0)
+		self.gene_info = TSS.merge(TTS)
+		self.prot = pd.read_table(path_init+'/prot.dat', header=0)['prot_pos']
+		self.env = pd.read_table(path_init+'/environment.dat')
+
+	def __str__(self):
+		s = "Genome Info :\n--------------\n"
+		s += "Size : "+str(self.Size)+"\n"
+		s += "Gene Info : \n"+str(self.gene_info)+"\n\n"
+		s += "Barriers : \n"+str(self.prot)+"\n\n"	
+		return s
+
+
+
 		
 		
+# class Gene:
+# 	"""docstring for Genome"""
+
+# 	def __init__(self, s,e,ori,prom_str):
+# 		self.s = s
+# 		self.e = e
+# 		self.ori = ori
+# 		self.prom_str = prom_str
