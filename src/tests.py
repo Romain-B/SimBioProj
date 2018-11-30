407cf922d14a93061068acc0796e34dc76500f15
Romain-B <rbulteau@bs406-s33-01.insa-lyon.fr>
2018-11-30 09:43:23 +0100

    Tests for fitness mod

--- src/tests.py
+++ src/tests.py
@@ -9,6 +9,6 @@ import matplotlib.pyplot as plt
 
 G = Genome(30000, '../sim_files/init')
 
-[G.run_generation_no_events('../sim_files/current') for i in range(100)]
+[G.run_generation_no_events('../sim_files/current') for i in range(10)]
 [G.run_generation('../sim_files/current') for i in range(100)]
 plt.show(block=True)
