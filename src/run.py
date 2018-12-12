e7a9172dd42f3d16c9239b626a26cd2a6133d109
Gonthier Solene <solene.gonthier@insa-lyon.fr>
2018-12-12 09:44:31 +0100

    add best_genome.gb in run

--- src/run.py
+++ src/run.py
@@ -4,13 +4,13 @@
 import numpy as np
 import pandas as pd
 from Genome import Genome
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 plan = pd.read_csv('../sim_files/mat_plan.csv', header = 0, sep=",")
 print(plan)
 
-len_noise = 2 # iterations without mutations to se the noise due to transcription
-len_simu = 3 # length of real simulation including mutations
+len_noise = 20 # iterations without mutations to se the noise due to transcription
+len_simu = 100 # length of real simulation including mutations
 
 #sec_dist=[40,80] #60
 indel_var=[50, 500] #0
@@ -24,13 +24,14 @@ nfails = 0
 for i, row in plan.iterrows():
   
   
-  G = Genome(30000, '../sim_files/init', sec_dist=sec_dist[row['sec_dist']], indel_var=indel_var[row['indel_var']], 
+  G = Genome(30000, '../sim_files/init', indel_var=indel_var[row['indel_var']], 
   	  p_inv=p_inv[row['p_inv']], T0=T0[row['T0']], nb_pol=nb_pol[row['nb_pol']], path_output = '../sim_files/output/'+row['alias'], NO_PLOT = True)
   [G.run_generation_no_events('../sim_files/current') for i in range(len_noise)]
 
   try :
     [G.run_generation('../sim_files/current') for i in range(len_simu)]
     # to see how look like the best genome we made
+    G.write_gb_file("../sim_files/output/"+row['alias']+"/best_genome.gb")
     l =['Best genome found\n\n', str(G.best[0]), 
         '\n\nBarriers positions\n\n', str(G.best[1]), 
         '\n\nBest fitness found\n\n', str(G.best_fit)]
