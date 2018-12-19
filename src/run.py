e78b20c20be54ce6defd59677862e1a14d62fb39
Gonthier Solene <solene.gonthier@insa-lyon.fr>
2018-12-19 09:08:22 +0100

    real modif

--- src/run.py
+++ src/run.py
@@ -10,23 +10,23 @@ plan = pd.read_csv('../sim_files/mat_plan.csv', header = 0, sep=",")
 print(plan)
 
 len_noise = 20 # iterations without mutations to se the noise due to transcription
-len_simu = 100 # length of real simulation including mutations
 
 #sec_dist=[40,80] #60
 indel_var=[50, 500] #0
 p_inv=[0.05, 0.2] #0.1
 T0=[0.001, 0.01] # 0.001
 nb_pol = [5, 9] #6
+nb_iter = [250, 500] # length of real simulation including mutations
 
 #G = Genome(30000, '../sim_files/init')
 nfails = 0
- 
+
 for i, row in plan.iterrows():
   
   
   G = Genome(30000, '../sim_files/init', indel_var=indel_var[row['indel_var']], 
   	  p_inv=p_inv[row['p_inv']], T0=T0[row['T0']], nb_pol=nb_pol[row['nb_pol']], path_output = '../sim_files/output/'+row['alias'], NO_PLOT = True)
-  [G.run_generation_no_events('../sim_files/current') for i in range(len_noise)]
+  [G.run_generation_no_events('../sim_files/current') for i in range(nb_iter['nb_iter'])]
 
   try :
     [G.run_generation('../sim_files/current') for i in range(len_simu)]
