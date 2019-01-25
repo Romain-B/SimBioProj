360f06031ea9309c5fe4c9a04ed7c43327896c3e
Romain-B <romain.bulteau@insa-lyon.fr>
2019-01-25 08:56:41 +0100

    Cleaned up code and commented all functions

--- src/draw_genome.py
+++ src/draw_genome.py
@@ -8,6 +8,13 @@ import sys
 
 
 def plot_genome(path_to_gb):
+  """
+  Takes a path to the genbank file.
+  Plots the genome in given gb file.
+
+  This function is based on an example given in the biopython tutorial/cookbook.
+  See http://biopython.org/DIST/docs/tutorial/Tutorial.html#htoc264 for original example.
+  """
   record = SeqIO.read(path_to_gb, "genbank")
 
   gd_diagram = GenomeDiagram.Diagram(record.id)
@@ -29,17 +36,14 @@ def plot_genome(path_to_gb):
 
   #for feature in record.features:
     if feature.type == "barrier":
-          #Exclude this feature
-          #continue
       gd_feature_set.add_feature(feature, sigil="BOX", 
                                  color="brown", line_width=30, strand=None)
 
 
-  gd_diagram.draw(format="circular", circular=True, pagesize=(20*cm,20*cm),
+  gd_diagram.draw(format="circular", circular=True, pagesize=(10*cm,10*cm),
                   start=0, end=len(record), circle_core = .8)
   gd_diagram.write(path_to_gb[:-2]+"pdf", "PDF")
-#gd_diagram.write("plasmid_circular_nice.eps", "EPS")
-#gd_diagram.write("plasmid_circular_nice.svg", "SVG")
+
 
 if __name__ == '__main__':
   plot_genome(sys.argv[1])
